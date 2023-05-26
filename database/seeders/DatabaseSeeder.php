8f9a1f81da89b2fc62c998c76181e23509af2d42
naser-da <naserdakhel@protonmail.com>
2023-05-27 01:29:51 +0300

    Add factories and model relationships

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,9 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\User;
+use App\Models\Link;
+
 
 class DatabaseSeeder extends Seeder
 {
@@ -12,7 +15,32 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
+        $users = User::factory(10)->create();
+
+        foreach ($users as $user) {
+            Link::factory(5)->create([
+                'user_id' => $user->id,
+            ]);
+        }
+
+        $users = User::factory(20)->create();
+
+        foreach ($users as $user) {
+            Link::factory(2)->create([
+                'user_id' => $user->id,
+            ]);
+        }
+
+        $users = User::factory(30)->create();
+
+        foreach ($users as $user) {
+            Link::factory(1)->create([
+                'user_id' => $user->id,
+            ]);
+        }
+
+        $users = User::factory(5)->create();
+
 
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
